9612071f835f5ad04d2356c3da6cac08b6968c3c
Will Budic <vilijanac@gmail.com>
2022-07-22 09:34:59 +1000

    Added test.

--- tests/testIPRange.pl
+++ tests/testIPRange.pl
@@ -4,8 +4,13 @@ use warnings; use strict; use Syntax::Keyword::Try; use Term::ANSIColor qw(:cons
 my $test=1;
 
 try{
-    die &failed if checkIPrange('192.168.0.1', '192.168.*.');   ++$test;
+    #
+    # Test common encounters.
+    die &failed if checkIPrange('192.168.0.1', '192.168.*.')==1;   ++$test;
     die &failed if checkIPrange('192.168.0.1', '192.168.*.*')==0;  ++$test;
+    die &failed if checkIPrange('192.168.0.1', '192.167.*.');   ++$test;
+    die &failed if checkIPrange('192.168.0.1', '192.167.*.*');   ++$test;
+    die &failed if !checkIPrange('192.168.0.1', '192.168.*.*');   ++$test;
     # It will die if it reports not in range. Here it returns true or 1.
     die &failed if !checkIPrange('192.168.0.1', '*.*.*.*');  ++$test;
     # $bool as 1 is true anything other is false: following $bool should be true, as the ranging matches.
@@ -25,16 +30,20 @@ try{
     #
     print BOLD "All tests have ", BRIGHT_GREEN ,"PASSED",RESET," for test file:", RESET WHITE, " $0\n", RESET;
 }
-catch{
-    my $failed = $@;
-    print BOLD BRIGHT_RED "Test file failed $failed";
+catch{ 
+    my ($failed, $coment, $past) = $@;    
     my @parse = ($failed =~ m/.*\s*at\s*(.*)\sline\s*(\d*)\.$/);
     open (my $file, '<:perlio', $parse[0]) or die("Error $! opening file: $parse[0]");
-    my ($coment, $past);
-    while( my $line = <$file>)  {       
+          my @slurp = <$file>;
+    close $file;
+    print BOLD BRIGHT_RED "Test file failed $failed";
+    our $DEC = "%0".(length($slurp[-1]) + 1)."d   ";
+    for(my $i=0; $i<@slurp;$i++)  { 
+        local $. = $i + 1;
+        my $line = $slurp[$i]; chomp $line;
         if($. >= $parse[-1]){
-           $line =~ s/^\s*//;
-           print $coment, "\n", RESET; 
+           $line =~ s/^\s*//; 
+           print $coment, RESET; 
            print "[".$parse[0]."]\n\t".BRIGHT_RED."Failed\@Line".WHITE." $. -> $line".RESET;
            last  
         }elsif($line=~m/^\s*(\#.*)/){
@@ -42,20 +51,22 @@ catch{
                 $past = 0 
             }
             elsif($past){
-                $coment .= "\n".RESET."$.   ".ITALIC.YELLOW.$1 
+                $coment .= "\n".RESET.frmln($.).ITALIC.YELLOW.$1 
             }
             else{
-                $past = 1;$coment = RESET."$.   ".ITALIC.YELLOW.$1 
+                $past = 1;$coment = RESET.frmln($.).ITALIC.YELLOW.$1 
             }
-
         }elsif($past){
                $line=~s/^\s*//;
                chomp($line);
-               $coment .= "\n".RESET."$.   ".$line; 
+               $coment .= "\n".RESET.frmln($.).$line; 
         }
     }
-    close $file;
 }
+    our $DEC =  "%-2d %s"; #under 100 lines pad like -> printf "%2d %s", $.
+    sub frmln { my($at)=@_;
+       return sprintf($DEC,$at)
+    }
 
     sub failed {
         my $err=shift; $err="" if !$err;
