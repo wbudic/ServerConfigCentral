a9cd92f7bd1326f267a6aef0c89b2b6b0021516d
Will Budic <vilijanac@gmail.com>
2022-07-27 23:23:43 +1000

    dev

--- client.pl
+++ client.pl
@@ -7,13 +7,15 @@ require CNFCentral;
 
 my $central = CNFCentral -> client();
 my $socket  = $central   -> {'socket'};
-my $cmd;
+my ($cmd,$strip);
 $socket->recv($cmd, 64);
-print "Connected: $cmd"; $cmd="";
+print "Connected: $cmd" if $central->config()->{'$DEBUG'}; 
+$cmd="" ;
 
 foreach my $a (@ARGV){
     my $v = $a; $v =~ s/^-+.*[=:]//;    
     if($a =~ m/^-+c/i)       {issueCommand($v) if $a ne $v}
+    elsif($a =~ m/^-+s/i)    {$strip =1;next}
     elsif($a =~ m/^-+p/i)    {$cmd = "prp "; $cmd = $cmd .$v  if $a ne $v}
     elsif($a =~ m/^-+h|\?/i) {&printHelp;}
     elsif($a !~ m/^-+/)      {
@@ -32,19 +34,31 @@ foreach my $a (@ARGV){
     $socket  = $central   -> socketC() if !$socket;
 }
 
-    #$socket=$central->socketC();
 if ($socket){
     $socket->send('end');
     $socket->close()
 }
 
 sub issueCommand { my ($cmd) = @_;
-    print "IssueCommand -> $cmd \n" if $central->config()->{'$DEBUG'};
+    print "IssueCommand -> $cmd\n" if $central->config()->{'$DEBUG'};
     try{ 
         $socket->send($cmd);
         my $read; my $buffer="";        
         while(sysread $socket, $read, 1024){ $buffer .= $read}
-        print "Received: $buffer\n";
+        if($strip){
+            #m/<<(.*?)<(.*?)>(\s*.*|[\*\.]*)>>>/gs          
+           #my @tag = ($buffer =~ m/<<(.*?)<(.*?)>(.*)>>+/gs); # $central-> tagCNFToArray($buffer);
+            my @tag = $central-> tagCNFToArray($buffer);
+
+           if(scalar @tag>1){
+              $strip = $tag[2]; $strip =~ s/^\s*|\s*$//g;
+              print $strip;
+           }else{
+              print "$buffer\n";
+           }
+        }else{
+            print "Received: $buffer\n";
+        }
     }catch{
         print "Socket Error -> ".$@;
     }
@@ -66,6 +80,8 @@ Options:
 -c='{command}'' -c... -         - Command to issue.
 -p=name {repository path}       - Fetches propery by name from an available repoistory config file.
 ./client.pl "{direct command}"  - Default behaviour is to issue command from the command line.
+-s                              - Strip CNF response tag, including header.
+                                  Translates property output value only if placed before command.
 --h -help_Me -? --help          - Prints this help. Perl trickery for the wickery.
 
 Commands:
